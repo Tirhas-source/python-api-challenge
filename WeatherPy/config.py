a467437bc71b8aff7c5449fd1f1fda59ee873c2b
Tirhas-source <56926891+Tirhas-source@users.noreply.github.com>
2021-05-12 01:40:30 -0400

    commit

---
+++ WeatherPy/config.py
@@ -0,0 +1,2 @@
+g_key = 'AIzaSyCKWaRCRJwLO14DFafu9-U5D_uVD_8g4iY'
+weather_api_key = 'da7023607babfcda2533cb107c9718b1'
---
+++ starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key =  "7fc379df0bced6d94e2426518df25660" 
+
+# Google API Key
+g_key = "7fc379df0bced6d94e2426518df25660" 
